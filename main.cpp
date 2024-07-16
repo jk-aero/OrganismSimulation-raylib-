4a92b304e808b64d13a1329864511fd5266bee69
jk-aero <92267875+jk-aero@users.noreply.github.com>
2024-07-17 00:48:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <string>
 
+//screen height and width
+const int screenWidth = 1000;
+const int screenHeight = 800;
+
+
 
 // Function to generate and return a random integer within a specified range
 int getRandomInt(int max, int min = 0) {
@@ -38,8 +43,8 @@ public:
 
     // Member functions (methods) to interact with the organism
     void move(int newX, int newY) {
-        posX = newX;
-        posY = newY;
+        posX += newX;
+        posY += newY;
     }
 
     void consumeEnergy(double amount) {
@@ -57,10 +62,11 @@ class Food {
 private:
     int size;       // Size of the food (assuming size is in some unit)
     int energy;     // Energy content of the food (assuming energy is in some unit)
-
+    
 public:
+    int posy, posx;
     // Constructor with default values
-    Food(int size = 0, int energy = 0) : size(size), energy(energy) {}
+    Food(int size, int posy, int posx, int energy = 0) : size(size), posy(posy), posx(posx),energy(energy) {}
 
     // Getter and setter methods
     int getSize() const {
@@ -80,76 +86,85 @@ public:
     }
 
     // Other methods if needed
-    void printDetails() const {
-        std::cout << "Food Size: " << size << ", Energy: " << energy << std::endl;
+    void printFood() const {
+        DrawCircle(posx, posy, size, PINK);
     }
+}
+;
 
-
-int main(void)
-{
-    const int screenWidth = 1000;
-    const int screenHeight = 800;
-
-
-
-    std::vector<Organism> totalOrganisms;
-    std::vector<food>totalFoods;
-    int organismCount(30);
-    int foodCount = 80;
-    for (int i=0;i<organismCount;i++)
+    int main(void)
     {
-        int size = getRandomInt(10,5);
-        int energy = 100;
-        int x = getRandomInt(screenWidth);
-        int range = getRandomInt(30,size+5);
-        int y = getRandomInt(screenHeight);
-        
-        totalOrganisms.push_back(Organism(size, x, y,range, energy));
-    
-    
-    }
-
-    for (int i = 0; i < foodCount; i++)
-    {
-        totalFood.push_back()
-
-
-    }
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-   
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-       
 
 
 
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        DrawCircle(150, 50, 10.0, RED);
-
-        for (Organism one : totalOrganisms)
+        std::vector<Organism> totalOrganisms;
+        std::vector<Food>totalFoods;
+        int organismCount(30);
+        int foodCount = 80;
+        for (int i = 0; i < organismCount; i++)
         {
-            DrawCircle(one.posX,one.posY, one.size, ORANGE);
-            DrawCircleLines(one.posX, one.posY, one.range, ORANGE);                         // Draw circle outline
+            int size = getRandomInt(10, 5);
+            int energy = 100;
+            int x = getRandomInt(screenWidth);
+            int range = getRandomInt(30, size + 5);
+            int y = getRandomInt(screenHeight);
+
+            totalOrganisms.push_back(Organism(size, x, y, range, energy));
+
 
         }
 
+        for (int i = 0; i < foodCount; i++)
+        {
+            int x = getRandomInt(screenWidth);
+            int y = getRandomInt(screenHeight);
+            int size = getRandomInt(5, 3);
+            totalFoods.push_back(Food(size,y,x, size * 5));
+        }
 
-                               
-        ClearBackground(RAYWHITE);
-        EndDrawing();
+        InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+        SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+
+
+        // Main game loop
+        while (!WindowShouldClose())    // Detect window close button or ESC key
+        {
+            // Update
+            //----------------------------------------------------------------------------------
+
+
+
+
+            //----------------------------------------------------------------------------------
+            BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawCircle(150, 50, 10.0, RED);
+
+            for (Organism one : totalOrganisms)
+            {
+                int x = getRandomInt(1,0);
+                int y = getRandomInt(1,0);
+                one.move(x, y);
+                DrawCircle(one.posX, one.posY, one.size, ORANGE);
+                DrawCircleLines(one.posX, one.posY, one.range, ORANGE);     
+               // Draw circle outline
+
+            }
+            for (Food sigleFood : totalFoods)
+            {
+                sigleFood.printFood();                         // Draw circle outline
+
             }
 
-       CloseWindow(); 
-    return 0;
-}
+
+
+            ClearBackground(RAYWHITE);
+            EndDrawing();
+        }
+
+        CloseWindow();
+        return 0;
+    }
